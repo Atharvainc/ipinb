810f9f25e495137c353132a25b6c958b16785338
Atharvainc <atharva.galande3005@gmail.com>
2026-07-18 01:02:33 +0530

    solved 2 ques, more tomorrow

--- dsa/pithon.py
+++ dsa/pithon.py
@@ -1,21 +1,17 @@
 import sys
 input = sys.stdin.readline
-
 def main():
     t=int(input())
+    candy=[]
+    for i in range(2,5000):
+        if len(set(str(i)))<=2:
+            candy.append(i)
     while t>0:
-        cnt=0
         n=int(input())
-        nums=list(map(int,input().split()))
-        for i in range(n-1):
-            if nums[i]==0:
+        for c in candy:
+            if len(set(str(c*n)))<=2:
+                print(c)
                 break
-            else:
-                nums[nums[i]-1]=0
-                cnt+=1
-        print(cnt)
         t-=1
-        
 if __name__=='__main__':
     main()
-        
